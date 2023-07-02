fb58699e9e80c5af1d9526ca4d3a9999a898bd25
Cheonse <108046230+cheonseunghyeon@users.noreply.github.com>
2023-07-02 12:33:48 +0900

    2주차 장고 개발

---
+++ mysite/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+from django.shortcuts import render
+from .models import MainContent
+def index(request):
+
+ # - 이 붙었으니 역순으로 정렬
+ content_list = MainContent.objects.order_by('-pub_date')
+ context = {'content_list': content_list}
+ # render 함수는 mysite/content_list.html 파일에 content_list 데이터를 적용
+ return render(request, 'mysite/content_list.html', context)
