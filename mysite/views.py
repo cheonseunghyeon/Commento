17df062f0c938509e3f154a1fe4d09a674826052
Cheonse <108046230+cheonseunghyeon@users.noreply.github.com>
2023-07-17 23:50:58 +0900

    4주차 개발 진행중

--- mysite/views.py
+++ mysite/views.py
@@ -12,3 +12,8 @@ def index(request):
  context = {'content_list': content_list}
  # render 함수는 mysite/content_list.html 파일에 content_list 데이터를 적용
  return render(request, 'mysite/content_list.html', context)
+
+def detail(request, content_id):
+ content_list = MainContent.objects.get(id=content_id)
+ context = { 'content_list': content_list}
+ return render(request, 'mysite/content_detail.html', context)
